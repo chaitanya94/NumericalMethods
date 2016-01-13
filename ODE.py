98c2da0f373bc12c507e75eb020632fc2735333c
Chaitanya Prasad <chaitanya94@gmail.com>
2016-01-13 23:21:59 +0530

    Euler works + Test case

--- ODE.py
+++ ODE.py
@@ -8,12 +8,12 @@ class Euler(object):
         """
 
     def get_function(self, func):
-        function = func
+        self.function = func
 
     def calculate(self, kwargs):
         step_size = kwargs["step"]
         end_time = kwargs["end_time"]
-        x0 = kwargs["intial_x"]
+        x0 = kwargs["initial_x"]
         y0 = kwargs["initial_y"]
         iterations = end_time/step_size
 
@@ -21,7 +21,25 @@ class Euler(object):
         x_old = x0
         y_new = 0
         for n in range(0, iterations):
-            y_new = y_old + step_size*function(x_old, y_old)
+            y_new = y_old + step_size*self.function(x_old, y_old)
             y_old = y_new
             x_old = x_old + step_size
         return y_new
+
+def test():
+    euler = Euler()
+
+    @euler.get_function
+    def test_func(x, y):
+        return x
+
+    step = 1
+    end_time = 10
+    initial_x = 0
+    initial_y = 0
+    ans = euler.calculate(locals())
+    print ans
+    print euler.help()
+
+if __name__ == '__main__':
+    test()
